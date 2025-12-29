f6e8415c8b1ddba0fe7d58a23dfb10771dcae9ac
eblantare <romain.blantare@gmail.com>
2025-12-29 06:04:16 +0000

    Remove backend files from frontend repository

--- events/urls.py
+++
@@ -1,9 +0,0 @@
-# events/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.events_list_api, name='events-list-api'),
-    path('<slug:slug>/', views.event_detail_api, name='event-detail-api'),
-    path('category/<str:category>/', views.events_by_category_api, name='events-by-category'),
-]
--- media/urls.py
+++
@@ -1,6 +0,0 @@
-# media/urls.py - VERSION MINIMALE
-from django.urls import path
-
-urlpatterns = [
-    # URLs temporairement vides
-]
